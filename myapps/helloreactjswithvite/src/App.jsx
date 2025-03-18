fed384d39e7c7e8d4de9310f1510bec9f63ce02d
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-03-18 17:45:48 +1100

    Trigger CD Test #10

--- myapps/helloreactjswithvite/src/App.jsx
+++ myapps/helloreactjswithvite/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <ExampleComponent text='hello from my-shared-ui!' />
-      <p>CD Trial #9</p>
+      <p>CD Trial #10</p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
