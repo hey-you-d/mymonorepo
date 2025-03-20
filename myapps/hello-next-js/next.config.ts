ceb10a9a76021c03b687c8129f4724086f41e591
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-03-20 21:33:31 +1100

    Configuration to accommodate the current ALB rule listener

--- myapps/hello-next-js/next.config.ts
+++ myapps/hello-next-js/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/hello-next-js',
+  output: "standalone",
+  env: {
+    PORT: '3000',
+  },
 };
 
 export default nextConfig;
