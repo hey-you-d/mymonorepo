387ce89f46f966a2b787956c7c2c1e45b2665117
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-03-31 13:21:53 +1100

    helloreactjswithvite - tidy up homepage

--- myapps/helloreactjswithvite/src/App.tsx
+++ myapps/helloreactjswithvite/src/App.tsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { ExampleComponent } from 'my-shared-ui'
-import { UserPage } from './views/UserPage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import MvvmPatternFetchPage from './pages/mvvmPatternFetch'
+import ExampleSharedUI from './pages/exampleSharedUI'
+import ExampleBasicRedux from './pages/exampleBasicRedux'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>This is H1 - CI test #1</h1>
-      <ExampleComponent text='hello from my-shared-ui!' />
-      <UserPage userId="2" />
+      <h1>Hello-React-JS</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,9 +30,21 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <a href="/hello-next-js" target="_self">To Hello-Next-JS</a>
+      <Router>
+        <nav>
+          <ul>
+            <li className="read-the-docs"><Link to="/mvvm-pattern-fetch">MVVM Pattern - data fetch example</Link></li>
+            <li className="read-the-docs"><Link to="/example-shared-ui">Example - shared library</Link></li>
+            <li className="read-the-docs"><Link to="/example-basic-redux">Example - basic redux</Link></li>
+            <Routes>
+              <Route path="/mvvm-pattern-fetch" element={<MvvmPatternFetchPage />} />
+              <Route path="/example-shared-ui" element={<ExampleSharedUI />} />
+              <Route path="/example-basic-redux" element={<ExampleBasicRedux />} />
+            </Routes>
+          </ul>
+        </nav>
+      </Router>
     </>
   )
 }
