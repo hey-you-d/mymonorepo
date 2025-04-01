7a4b047293c1fa494bc8f613e44001207a615757
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-04-01 12:06:10 +1100

    helloreactjswithvite - Move basic redux example from hello-next-js

--- myapps/helloreactjswithvite/src/main.tsx
+++ myapps/helloreactjswithvite/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { reduxStore } from './reduxStore'
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
