b8981052d5c28d8df767dde78699e70a60338a9a
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-04-04 21:12:31 +1100

    hello-next-js - optimised dockerfile part 9

--- myapps/hello-next-js/next.config.ts
+++ myapps/hello-next-js/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "standalone",
     assetPrefix: '/hello-next-js',
-    trailingSlash: true,
+    // prevent 308 redirects on API routes. This is needed for the standalone mode's /api/health to work 
+    trailingSlash: false, 
 };
 
 export default nextConfig;
