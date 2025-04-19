66cd33558dae7d719349b7e8f719ef1f742b37df
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-04-19 19:19:13 +1000

    hello-next-js - optimise dockerfile with standalone mode

--- myapps/hello-next-js/middleware.ts
+++ myapps/hello-next-js/middleware.ts
@@ -1,8 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const prefix = '/hello-next-js';
+
 export const middleware = (req: NextRequest) => {
     const url = req.nextUrl;
-    const prefix = '/hello-next-js';
+    const hostname = req.headers.get('host') || '';
+    const isLocalhost = hostname.startsWith('localhost') || hostname.startsWith('127.0.0.1');
+    const isHttps = req.headers.get('x-forwarded-proto') === 'https';
+
+    /***
+     * handle healthcheck rewrite (ALB hits hello-next-js/api/health)
+     */
+    if (url.pathname === `${prefix}/api/health`) {
+        url.pathname = "/api/health";
+        return NextResponse.rewrite(url);
+    }
+
+    // re-evaluate isHealthCheck after potential rewrite
+    const isHealthCheck = isLocalhost 
+        ? url.pathname.startsWith('/api/health') 
+        : url.pathname.startsWith(`${prefix}/api/health`);
+
+    /***
+     * bypass next.js static assets
+     */
+    if (url.pathname.startsWith(`${prefix}/_next`) || 
+        url.pathname.startsWith(`${prefix}/static`)) {
+        return NextResponse.next();
+    }
+
+    // Redirect HTTP to HTTPS — except for health check
+    if (!isLocalhost && !isHttps && !isHealthCheck) {
+        url.protocol = "https:";
+        console.log('MIDDLEWARE DEBUG: redirecting to HTTPS');
+        return NextResponse.redirect(url, 301);
+    }
 
     /***
      * handle static assets (_next, static) so they load correctly
@@ -15,7 +47,7 @@ export const middleware = (req: NextRequest) => {
     }
 
     /*** 
-     * Goal: rewrite pages so...
+     * Goal: rewrite pages so... (recall, no basePath option in next.config.js)
      * / -> see the homepage
      * /hello-next-js -> rewritten to / -> see the homepage
      * /about -> see the about page
@@ -31,3 +63,18 @@ export const middleware = (req: NextRequest) => {
 
     return NextResponse.next();
 } 
+
+
+/*** 
+ * Only run the middleware for any path that starts with /hello-next-js/ (and everything under it).
+ * 
+ * Since this app is not using basePath anymore, but the hello-next-js app  is still served behind 
+ * a path prefix via ALB, the middleware needs to handle rewriting and asset loading cleanly only for 
+ * /hello-next-js/*.
+ * 
+ * This keeps your middleware from running on unrelated routes like / or /api/something from your other apps 
+ * (like hello-react-js). 
+ */
+export const config = {
+    matcher: ['/hello-next-js/:path*'],
+}
