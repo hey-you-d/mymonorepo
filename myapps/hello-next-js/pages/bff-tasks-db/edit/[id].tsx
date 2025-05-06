de6ecafb4ca77eab2c14ef201b36601338bb4914
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-05-06 11:13:28 +1000

    hello-next-js - Tasks BFF - refactor - part 3

--- myapps/hello-next-js/pages/bff-tasks-db/edit/[id].tsx
+++ myapps/hello-next-js/pages/bff-tasks-db/edit/[id].tsx
@@ -2,36 +2,22 @@ import { useRouter } from 'next/router';
 import Layout from "../../../components/Layout";
 import { TaskDetailPage } from "@/app/views/taskDetailPage";
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { SWRConfig } from 'swr';
-//import { Task } from '@/app/types/Task';
 
-//type Props = {
-//  fallback: Record<string, Task[]>;
-//};
-
-// dev note: alternatively...
-//const BffTasksDB = ({ fallback }: Props) => {
 const BffTasksDB = ({ fallback }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const router = useRouter();
     const { id } = router.query;
 
     return (
-        <SWRConfig value={{ fallback }}> 
-          <Layout title="hello-next-js - Next.JS API - Backend For Frontend (BFF) demo">               
-              <TaskDetailPage id={Number(id)} />
-              <br />
-          </Layout>
-        </SWRConfig>
+      <Layout title="hello-next-js - Next.JS API - Backend For Frontend (BFF) demo">               
+          <TaskDetailPage id={Number(id)} />
+          <br />
+      </Layout>
     );
 }
 
 const getServerSideProps: GetServerSideProps = async () => {
   return {
-    props: {
-      fallback: {
-        'Tasks-API': [],
-      },
-    },
+    props: {}, // skip SSR, that means getTasksDBRows() is called with useEffect (CSR approach)
   };
 };
 
