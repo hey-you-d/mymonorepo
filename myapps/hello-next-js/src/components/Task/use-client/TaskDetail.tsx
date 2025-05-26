f2734198cb192e8434cd70de785af35f9ae58957
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-05-26 13:54:01 +1000

    hello-next-js - refactor: Tasks List - frontend layer
    part 5

--- myapps/hello-next-js/src/components/Task/use-client/TaskDetail.tsx
+++ myapps/hello-next-js/src/components/Task/use-client/TaskDetail.tsx
@@ -2,17 +2,19 @@
 
 // The View (presentation component) is a pure functional component focused on displaying data and 
 // responding to user actions passed in as props.
-import React from 'react';
+import { Dispatch, SetStateAction, MouseEvent } from 'react';
 import { Task } from "@/types/Task";
 import { MONOREPO_PREFIX, TASKS_CRUD } from "@/lib/app/common";
 
-type TaskTableType = {
+type TaskDetailType = {
     row: Task,
     tasks: Task[] | undefined,
     deleteRowFromId: (id: number) => Promise<void>,
+    buttonDisabled: boolean,
+    setButtonDisabled: Dispatch<SetStateAction<boolean>>,
 }
 
-export const TaskDetail = ({ row, tasks, deleteRowFromId } : TaskTableType) => {
+export const TaskDetail = ({ row, tasks, deleteRowFromId, buttonDisabled, setButtonDisabled } : TaskDetailType) => {
     // for reference: the if condition below only applies to the non-graphql row deletion op.
     // the graphql version returns an updated tasks (sans the deleted row). 
     if (tasks && tasks.length <= 0) {
@@ -22,13 +24,29 @@ export const TaskDetail = ({ row, tasks, deleteRowFromId } : TaskTableType) => {
         window.location.href=`${MONOREPO_PREFIX}${TASKS_CRUD}`;
     }
 
+    const onClickHandler = async (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+        setButtonDisabled(true);
+        try { 
+            await deleteRowFromId(Number(row.id));
+            setButtonDisabled(false);
+        } catch(e) {
+            throw new Error(`Delete row ${row.id} failed: ${e}`);
+        }
+    }
+
+    const renderButton = buttonDisabled 
+        ? <button type="button" disabled>Delete this record</button>
+        : <button type="button" onClick={(e) => onClickHandler(e)}>Delete this record</button>
+
     return (
         <>
             <p>id: {row.id}</p>
             <p>title: {row.title}</p>
             <p>detail: {row.detail}</p>
             <p>completed? {row.completed ? "yes" : "no"}</p>
-            <div><button type="button" onClick={() => deleteRowFromId(Number(row.id))}>Delete this record</button></div>
+            <div>{renderButton}</div>
         </>
     );    
 };
--- myapps/hello-next-js/src/views/Task/use-client/taskDetailPage.tsx
+++ myapps/hello-next-js/src/views/Task/use-client/taskDetailPage.tsx
@@ -12,6 +12,7 @@ export const TaskDetailPage = ({id}: {id: number}) => {
   const { tasks, loading, deleteRowFromId } = useTaskViewModelWithSwr();
 
   const [row, setRow] = useState<Task | undefined>(undefined);
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
 
   useEffect(() => {
     if (tasks) {
@@ -24,7 +25,13 @@ export const TaskDetailPage = ({id}: {id: number}) => {
 
   const body: React.ReactElement[] = []
   body.push(row 
-    ? <TaskDetail row={row} tasks={tasks} deleteRowFromId={deleteRowFromId} /> 
+    ? <TaskDetail 
+        row={row} 
+        tasks={tasks} 
+        deleteRowFromId={deleteRowFromId} 
+        buttonDisabled={buttonDisabled}
+        setButtonDisabled={setButtonDisabled} 
+      /> 
     : <p>{`The record ${id} is no longer exist`}</p>);
 
   body.push(<div><Link href={`${MONOREPO_PREFIX}/${TASKS_CRUD}`}>Back to the table page</Link></div>);  
