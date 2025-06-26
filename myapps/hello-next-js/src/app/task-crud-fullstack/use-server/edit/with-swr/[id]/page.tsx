e5cd609f7d1f92e30c3ab356b2f4128222faf09d
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-06-26 12:10:41 +1000

    hello-next-js feat: dynamic back link in the detail page
    part 4

--- myapps/hello-next-js/src/app/task-crud-fullstack/use-server/edit/with-swr/[id]/page.tsx
+++ myapps/hello-next-js/src/app/task-crud-fullstack/use-server/edit/with-swr/[id]/page.tsx
@@ -6,11 +6,7 @@ import Layout from "../../../../../../../pagesLayouts/Layout";
 import { SERVER_SIDE_FRONTEND_LAYOUT_TITLE } from "../../../../../../../pagesLayouts/layout-title";
 import { TaskDetailWithSwrPage } from "@/views/Task/use-server/taskDetailWithSwrPage";
 import { MONOREPO_PREFIX, TASKS_CRUD } from '@/lib/app/common';
-
-type Props = {
-    params: { id: string };
-    searchParams?: { [key: string]: string | string[] | undefined }; // to retrieve query param: &from=""
-};
+import type { AppRouterPageProps as Props } from '@/types/Task';
 
 // const TaskDetailNextJSPage = ({ params }: { params: { id: string } }) => {
 // for reference:
