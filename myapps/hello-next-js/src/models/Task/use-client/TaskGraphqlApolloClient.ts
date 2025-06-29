d522c56b80208b6266becc20608b0fabf49b5d2f
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-06-29 11:24:42 +1000

    hello-next-js bug: TASK CRUD - fix auth integration for the Apollo-Client module

--- myapps/hello-next-js/src/models/Task/use-client/TaskGraphqlApolloClient.ts
+++ myapps/hello-next-js/src/models/Task/use-client/TaskGraphqlApolloClient.ts
@@ -1,14 +1,18 @@
 "use client"
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { TASKS_API_RELATIVE_URL } from "@/lib/app/common";
+import { TASKS_BFF_RELATIVE_URL } from "@/lib/app/common";
 
 // for reference: absolute URI for SSR, relative (proper) or absolute (if no choice) URI for CSR
 // SSR: 'http://localhost:3000/api/tasks/v1/sql/graphql'
 // CSR: '/api/tasks/v1/sql/graphql'
 // for reference: can't be prepended with "/hello-next-js/" 
 const apolloClient = new ApolloClient({
-    uri: `${TASKS_API_RELATIVE_URL}/graphql`, 
+    uri: `${TASKS_BFF_RELATIVE_URL}/graphql`, 
     cache: new InMemoryCache(), // InMemoryCache setup
+    credentials: 'include', // This is equivalent to credentials: 'include' in fetch
+    headers: {
+        'Content-Type': 'application/json', // Optional, Apollo sets this by default
+    }
 });
   
 export default apolloClient;
