6a4b0edd9be30387d5137d8cf2cd9437aa688de9
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-07-08 12:07:32 +1000

    hello-next-js: feat: TASK CRUD - supabase as prod db - part 2
    part 1

--- myapps/hello-next-js/pagesLayouts/Layout.tsx
+++ myapps/hello-next-js/pagesLayouts/Layout.tsx
@@ -1,11 +1,13 @@
 import { ReactElement, ReactNode } from 'react';
 import Link from 'next/link';
 import Footer from './Footer';
-import { TASKS_CRUD, DOMAIN_URL, TASKS_BFF_DOMAIN_API_URL, TASKS_SQL_DOMAIN_API_URL } from '@/lib/app/common';
+import { TASKS_CRUD, MONOREPO_PREFIX, TASKS_BFF_DOMAIN_API_URL, TASKS_SQL_DOMAIN_API_URL } from '@/lib/app/common';
 import "@/app/globals.css";
 import styles from "@/app/page.module.css";
 
 const Layout = ({ children, title } : { children:ReactNode | ReactElement, title: string }) => {
+    const finalPrefix = `${MONOREPO_PREFIX}${TASKS_CRUD}`;
+
     return (
         <section className={styles.page}>
             <header>
@@ -14,23 +16,23 @@ const Layout = ({ children, title } : { children:ReactNode | ReactElement, title
             <main className={styles.main}>
                 <h3>[Client-side components MVVM variant example pages]</h3>
                 <ul>
-                    <li><Link href={`${TASKS_CRUD}`}>Default (No Frills) example page</Link></li>    
-                    <li><Link href={`${TASKS_CRUD}/with-swr`}>Cached With Vercel SWR example page</Link></li>
-                    <li><Link href={`${TASKS_CRUD}/with-search-filter`}>useDeferredValue optimised filtering example page</Link></li>
-                    <li><Link href={`${TASKS_CRUD}/graphql`}>Data fetching with graphql (via Apollo Server) example page</Link></li>
-                    <li><Link href={`${TASKS_CRUD}/graphql/apolloClient`}>Data fetching & querying with graphql (via Apollo Server & Client) example page</Link></li>
+                    <li><Link href={`${finalPrefix}`}>Default (No Frills) example page</Link></li>    
+                    <li><Link href={`${finalPrefix}/with-swr`}>Cached With Vercel SWR example page</Link></li>
+                    <li><Link href={`${finalPrefix}/with-search-filter`}>useDeferredValue optimised filtering example page</Link></li>
+                    <li><Link href={`${finalPrefix}/graphql`}>Data fetching with graphql (via Apollo Server) example page</Link></li>
+                    <li><Link href={`${finalPrefix}/graphql/apolloClient`}>Data fetching & querying with graphql (via Apollo Server & Client) example page</Link></li>
                 </ul>
                 <h3>[Server-side components MVVM variant example pages]</h3>
                 <ul>
-                    <li><Link href={`${TASKS_CRUD}/use-server`}>Default (No Frills) example page</Link></li>
-                    <li><Link href={`${TASKS_CRUD}/use-server/with-swr`}>Cached With Vercel SWR example page</Link></li>
-                    <li><Link href={`${TASKS_CRUD}/use-server/graphql`}>Data fetching with graphql (via Apollo Server) example page</Link></li>
+                    <li><Link href={`${finalPrefix}/use-server`}>Default (No Frills) example page</Link></li>
+                    <li><Link href={`${finalPrefix}/use-server/with-swr`}>Cached With Vercel SWR example page</Link></li>
+                    <li><Link href={`${finalPrefix}/use-server/graphql`}>Data fetching with graphql (via Apollo Server) example page</Link></li>
                 </ul>
                 <h3>[Backend layer]</h3>
                 <ul>
-                    <li><Link href={`${TASKS_CRUD}/swagger`}>Swagger Doc Page</Link></li>
-                    <li><Link href={`${TASKS_SQL_DOMAIN_API_URL}`}>Tasks API Endpoints</Link></li>
-                    <li><Link href={`${TASKS_BFF_DOMAIN_API_URL}`}>Tasks BFF Endpoints (used by the client-side variant Model component)</Link></li>
+                    <li><Link href={`${finalPrefix}/swagger`}>Swagger Doc Page</Link></li>
+                    <li><Link href={"/api/tasks/v1/sql"}>Tasks API Endpoints</Link></li>
+                    <li><Link href={"/api/tasks/v1/bff"}>Tasks BFF Endpoints (used by the client-side variant Model component)</Link></li>
                 </ul>
                 <hr/>
                 {children}
