b4ca0ecb738ebfbf973120572c243d6a861d74a3
Yudiman Kwanmas <kwanmas.yudiman@outlook.com>
2025-07-10 14:18:07 +1000

    hello-next-js: feat: TASK CRUD - supabase as prod db - part 11-b

--- copilot/iam-get-role-policy.sh
+++ copilot/iam-get-role-policy.sh
@@ -1,3 +1,7 @@
 aws iam get-role-policy \
   --role-name mymonorepo-app-trial-1-production-mymonore-TaskRole-RT1UYAv3PGPJ \
   --policy-name AllowParameterStoreAccess
+
+aws iam get-role-policy \
+  --role-name mymonorepo-app-trial-1-development-mymonor-TaskRole-wMJxy6Ff33gq \
+  --policy-name AllowParameterStoreAccess
